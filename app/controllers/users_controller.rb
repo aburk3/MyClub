25e11626c95c863acf96e747d214e3a9656dbdc6
aburk3 <aburk3@gmail.com>
2019-02-26 17:42:36 -0600

    Change route for user signin page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
-class UsersController < ApplicationController
+require 'pry'
 
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   root "sessions#new"
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
 end
