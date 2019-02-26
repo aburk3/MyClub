5063c95619dde96ed34819a5f9ad73604a87bb2e
aburk3 <aburk3@gmail.com>
2019-02-25 21:06:14 -0600

    Changed welcome notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+      redirect_to user_path(@user), notice: "Welcome to bookVin!"
     else
       redirect_to signin_path
     end
