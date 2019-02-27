57628d279a3dcc9da99f89875aa97212d711cf18
aburk3 <aburk3@gmail.com>
2019-02-26 19:47:44 -0600

    Working on correction the User Club association

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,7 +1,7 @@
 class Club < ApplicationRecord
   has_many :users_clubs
-  has_many :clubs, :through => :users_clubs
-  
+  has_many :users, :through => :users_clubs
+
   has_many :meetings
   has_many :books, :through => :meetings
 end
--- db/migrate/20190226012618_create_users_clubs.rb
+++ db/migrate/20190226012618_create_users_clubs.rb
@@ -1,8 +1,8 @@
 class CreateUsersClubs < ActiveRecord::Migration[5.2]
   def change
     create_table :users_clubs do |t|
-      t.integer :user_id, null: false
-      t.integer :club_id, null: false
+      t.belongs_to :user_id, null: false
+      t.belongs_to :club_id, null: false
     end
   end
 end
