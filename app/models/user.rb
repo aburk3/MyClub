2a59735b531e4513851b448f0e95dd741658b70c
aburk3 <aburk3@gmail.com>
2019-02-27 19:12:46 -0600

    added error styling for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :user_clubs
   has_many :clubs, :through => :user_clubs
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
