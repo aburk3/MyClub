ca01fd9bfda7ee439da9e54bf4c473f106c83a60
aburk3 <aburk3@gmail.com>
2019-03-01 06:34:52 -0600

    Adding basic CRUD for MeetingsController

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,28 @@
 class MeetingsController < ApplicationController
+
+  def index
+    @meetings = Meeting.all
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    if @meeting.save
+
+
+      redirect_to @meeting, notice: "Meeting was succesfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+    def meeting_params
+      params.require(:club).permit(
+        :name,
+        :description,
+        :count,
+        :meeting_datetime
+      )
+    end
+
 end
