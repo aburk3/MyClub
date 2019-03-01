431f50038fb1d330c8b52330569b62b17528084e
aburk3 <aburk3@gmail.com>
2019-02-28 18:21:31 -0600

    Made the show page of each user dynamic based on the current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :clubs, :through => :user_clubs
   validates :first_name, presence: true, on: :create
   validates :last_name, presence: true, on: :create
-  validates :email, presence: true, on: :create
-  validates :password, presence: true, on: :create
+  validates :email, uniqueness: true, presence: true, on: :create
+  validates :password, presence: true, :length => {:within => 6..40}, on: :create
 end
