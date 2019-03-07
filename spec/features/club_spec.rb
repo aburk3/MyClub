86a3d391056702f5c988beba9507751d2941b892
aburk3 <aburk3@gmail.com>
2019-03-07 06:16:42 -0600

    spec that fills out new club form complete

--- spec/features/club_spec.rb
+++ spec/features/club_spec.rb
@@ -13,6 +13,16 @@ describe 'new club' do
 
   it "displays a new club form that redirects to the show page, which then contains the submitted club's name and description" do
     visit new_club_path
-    fill_in
+    fill_in("club[name]", with: 'My club name')
+    fill_in("club[club_category]", with: 'My club club_category')
+    fill_in("club[description]", with: 'My club description')
+
+    click_on 'Submit'
+
+    expect(page.current_path).to eq(clubs_path)
+    expect(page).to have_content('My club name')
+    expect(page).to have_content('My club category')
+    expect(page).to have_content('My club description')
+  end
 
 end
