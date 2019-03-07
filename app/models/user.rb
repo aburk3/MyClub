ddbcc99717a130eb7948e30c2e5d627c98f945b4
aburk3 <aburk3@gmail.com>
2019-03-06 18:17:51 -0600

    Added validation for for email using format URI

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :clubs, :through => :user_clubs
   validates :first_name, presence: true, on: :create
   validates :last_name, presence: true, on: :create
-  validates :email, uniqueness: true, presence: true, on: :create
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, on: :create
   validates :password, presence: true, :length => {:within => 6..40}, on: :create
   validates :password, presence: true, :length => {:within => 6..40}, on: :update
 
